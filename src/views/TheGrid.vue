1a89fc81085f815ecaba8daf38ce92e840cfbee5
Eduardo Sacco <saccoeduardo@gmail.com>
2024-08-08 09:26:44 -0300

    add x and instagram poc

--- src/views/TheGrid.vue
+++ src/views/TheGrid.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import { GridLayout } from 'grid-layout-plus'
+import InstagramEmbed from '@/components/InstagramPost.vue'
+import XPost from '@/components/XPost.vue'
+
+const draggable = ref(true)
+const resizable = ref(false)
+const responsive = ref(true)
 
 const layout = reactive([
   {
-    x: 0,
+    x: 3,
     y: 0,
-    w: 6,
-    h: 10,
+    w: 5,
+    h: 20,
     i: '0',
     static: false,
     data: {
+      type: 'iframe',
       width: '100%',
       height: '100%',
       src: 'https://www.youtube.com/embed/E1FNkf3MLKY?si=2fWw28HI1xtv8_wA',
@@ -22,19 +29,46 @@ const layout = reactive([
     }
   },
   {
-    x: 6,
-    y: 0,
-    w: 6,
-    h: 10,
+    x: 3,
+    y: 20,
+    w: 5,
+    h: 19,
     i: '1',
     static: false,
     data: {
+      type: 'iframe',
       width: '100%',
       height: '100%',
       src: 'https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO0nT692?utm_source=generator',
       title: 'Spotify playlist',
       frameborder: '0',
       allow: 'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
+    }
+  },
+  {
+    x: 0,
+    y: 0,
+    w: 3,
+    h: 27,
+    i: '2',
+    static: false,
+    data: {
+      type: 'instagram_post',
+      width: '100%',
+      height: '100%'
+    }
+  },
+  {
+    x: 8,
+    y: 0,
+    w: 3,
+    h: 31,
+    i: '3',
+    static: false,
+    data: {
+      type: 'x_post',
+      width: '100%',
+      height: '100%'
     }
   }
 ])
@@ -43,19 +77,44 @@ const layout = reactive([
 <template>
   <main>
     <h1>The Grid</h1>
-    <GridLayout v-model:layout="layout" :row-height="30">
-      <template #item="{ item }">
-        <iframe
-          :src="item.data.src"
-          :width="item.data.width"
-          :height="item.data.height"
-          :title="item.data.title"
-          :frameborder="item.data.frameborder"
-          :allow="item.data.allow"
-          :referrerpolicy="item.data.referrerpolicy"
-        ></iframe>
-      </template>
-    </GridLayout>
+    <div class="layout-json">
+      Displayed as <code>[x, y, w, h]</code>:
+      <div class="columns">
+        <div v-for="item in layout" :key="item.i">
+          <strong>{{ item.i }}</strong
+          >: [{{ item.x }}, {{ item.y }}, {{ item.w }}, {{ item.h }}]
+        </div>
+      </div>
+    </div>
+    <hr />
+    <input v-model="draggable" type="checkbox" /> Draggable
+    <input v-model="resizable" type="checkbox" /> Resizable
+    <input v-model="responsive" type="checkbox" /> Responsive
+    <br />
+    <div style="margin-top: 10px">
+      <GridLayout
+        v-model:layout="layout"
+        :row-height="10"
+        :is-draggable="draggable"
+        :is-resizable="resizable"
+        :responsive="responsive"
+      >
+        <template #item="{ item }">
+          <iframe
+            v-if="item.data.type === 'iframe'"
+            :src="item.data.src"
+            :width="item.data.width"
+            :height="item.data.height"
+            :title="item.data.title"
+            :frameborder="item.data.frameborder"
+            :allow="item.data.allow"
+            :referrerpolicy="item.data.referrerpolicy"
+          ></iframe>
+          <InstagramEmbed v-if="item.data.type === 'instagram_post'" />
+          <XPost v-if="item.data.type === 'x_post'" />
+        </template>
+      </GridLayout>
+    </div>
   </main>
 </template>
 
@@ -85,5 +144,16 @@ const layout = reactive([
   margin: auto;
   font-size: 24px;
   text-align: center;
+}
+
+.layout-json {
+  padding: 10px;
+  margin-top: 10px;
+  background-color: #ddd;
+  border: 1px solid black;
+}
+
+.columns {
+  columns: 120px;
 }
 </style>
