7d4e4bb53c32a27d6d9787e02240fceed18c0c1b
Eduardo Sacco <saccoeduardo@gmail.com>
2024-08-14 15:04:16 -0300

    Apply Fabian's feedback

--- src/views/TheGrid.vue
+++ src/views/TheGrid.vue
@@ -2,13 +2,15 @@
 import { reactive, ref } from 'vue'
 import { GridLayout } from 'grid-layout-plus'
 
+const ROW_HEIGHT = 10
+
 const draggable = ref(true)
 const resizable = ref(false)
-const responsive = ref(true)
+const responsive = ref(false)
 
 const layout = reactive([
   {
-    x: 3,
+    x: 5,
     y: 0,
     w: 5,
     h: 20,
@@ -27,10 +29,10 @@ const layout = reactive([
     }
   },
   {
-    x: 3,
+    x: 5,
     y: 20,
     w: 5,
-    h: 19,
+    h: 31,
     i: '1',
     static: false,
     data: {
@@ -44,10 +46,10 @@ const layout = reactive([
     }
   },
   {
-    x: 0,
+    x: 2,
     y: 0,
     w: 3,
-    h: 27,
+    h: 22,
     i: '2',
     static: false,
     data: {
@@ -60,10 +62,10 @@ const layout = reactive([
     }
   },
   {
-    x: 8,
-    y: 0,
+    x: 2,
+    y: 22,
     w: 3,
-    h: 31,
+    h: 29,
     i: '3',
     static: false,
     data: {
@@ -85,7 +87,8 @@ const layout = reactive([
 
 :deep(.vgl-item:not(.vgl-item--placeholder)) {
   background-color: #ccc;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  border-radius: 12px;
+  /* box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); */
 }
 
 :deep(.vgl-item--resizing) {
@@ -132,22 +135,25 @@ const layout = reactive([
     <div class="mt-2">
       <GridLayout
         v-model:layout="layout"
-        :row-height="10"
+        :row-height="ROW_HEIGHT"
         :is-draggable="draggable"
         :is-resizable="resizable"
         :responsive="responsive"
       >
         <template #item="{ item }">
-          <iframe
-            v-if="item.data.type === 'iframe'"
-            :src="item.data.src"
-            :width="item.data.width"
-            :height="item.data.height"
-            :title="item.data.title"
-            :frameborder="item.data.frameborder"
-            :allow="item.data.allow"
-            :referrerpolicy="item.data.referrerpolicy"
-          ></iframe>
+          <div slot="content" class="flex h-full p-4 rounded-xl shadow-xl bg-white">
+            <iframe
+              class="rounded-xl overflow-hidden"
+              v-if="item.data.type === 'iframe'"
+              :src="item.data.src"
+              :width="item.data.width"
+              :height="item.data.height"
+              :title="item.data.title"
+              :frameborder="item.data.frameborder"
+              :allow="item.data.allow"
+              :referrerpolicy="item.data.referrerpolicy"
+            ></iframe>
+          </div>
         </template>
       </GridLayout>
     </div>
